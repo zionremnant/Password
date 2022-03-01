dce1637dd4b072aa94f874f914b9a830945f8995
zionremnant <99617307+zionremnant@users.noreply.github.com>
2022-02-28 19:14:44 -0800

    add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// user clicks button then prompt pops up asking for number of characters
+
+// Do i need to hold any information?
+// create array with special characters
+// create array with numeric, 
+// create array lowercase
+// create array upper case
+
+// create empty array that holds all the true confirmed options in it.
+// push all character that user wants into this array. This creates the pool of characters to select from
+// once the pool is complete, then randomly select characters based on the number of characters the user selcted in the beggining.
+
+// generate password function
+// var numberOfChar = validatecharLength(prompts for number of characters) 
+// var isSpecialCharaters = confirm pops up asking if they would like specialial characters
+// var isCapitol = confirm for capital, 
+// var isLowercase = lowercase confirm, 
+// var isNumeric = numeric characters confirm
+
+// validate number of chars is between 8 and 128
+// validator function send it users input
+
+function validateCharLength (numberOfChars) {
+   if (numberOfChars < 8 || numberOfChars > 128) {
+     alert("Please enter a number between 8 and 128");
+     return generatePassword()
+   }
+   return numberOfChars
+ }
+
+// validate user selected at least one of the availbale options
+// if user selected isSpecial or isCapital or isLower or isNum then continue
+// else alert user "none selected" and return to main function call
+
+
+
+
+
+
+
+
+// Assignment Code
+var userInput
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numeric = [0,1,2,3,4,5,6,7,8,9];
+var isSpecialCharaters = ["!","@","#","$","%","^","&","*"];
+
+if (confirm("Does your password"))
+
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
