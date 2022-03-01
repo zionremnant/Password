4c6b55cced9b82a85cbf84b06a01b0b1aaad05ef
Zion Yang <zionremnant@gmail.com>
2022-02-28 19:34:50 -0800

    added var

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ var capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","
 var numeric = [0,1,2,3,4,5,6,7,8,9];
 var isSpecialCharaters = ["!","@","#","$","%","^","&","*"];
 
-if (confirm("Does your password require lowercase letters??"))
+if (confirm("Does your password require lowercase?"))
 
 
 
