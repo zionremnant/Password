2b41398d87a9b0ebb115419925db4c2580cfcfde
Zion Yang <zionremnant@gmail.com>
2022-03-02 01:12:05 -0800

    concat

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
 var generateBtn = document.querySelector("#generate")
-generateBtn.addEventListener("click", writePassword);{
-};
+generateBtn.addEventListener("click", writePassword);
+function writePassword(){
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+}
 // Assignment Code
-
+var userInput = []
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = [0,1,2,3,4,5,6,7,8,9];
 var specialCharaters = ["!","@","#","$","%","^","&","*"];
 
+function generatePasswor(){
+  var password = " ";
+  numberOfChars = prompt("Please enter a number between 8 and 128")
+  if(numberOfChars < 8 || numberOfChars > 128) {
+    alert("Please enter a number between 8 and 128");
+    return generatePassword;
+  }
+  var lowercase = confirm("Would you like to include lowercase letters?");
+  var capital = confirm("Would you like to include capital letters?");
+  var numeric = confirm("Would you like to include numbers?");
+  var specialCharaters = confirm("Would you like to include special characters?");
+  if(lowercase){
+    userInput = userInput.concat(lowercase)
+  }
+  if(capital){
+    userInput = userInput.concat(capital)
+  }
+  if(numeric){
+    userInput = userInput.concat(numeric)
+  }
+  if(specialCharaters){
+    userInput = userInput.concat(specialCharaters)
+  }
 
-function generatePassword (){
-  var password = [];
-  var userInput = []
-  let numberOfChars = prompt("Please enter a number between 8 and 128");
-  validateCharLength(numberOfChars);
-
-  if(confirm("Would you like to include lowercase letters?")==true){
-    userInput.push(lowercase)
-  }
-  if(confirm("Would you like to include capital letters?")==true){
-    userInput.push(capital)
-  }
-  if(confirm("Would you like to include numbers?")==true){
-    userInput.push(numeric)
-  }
-  if(confirm("Would you like to include special characters?")==true){
-    userInput.push(specialCharaters)
-  }
-  
-console.log(userInput);
+console.log(numberOfChars);
 
   for (var i = 0; i < parseInt(numberOfChars); i++){
-    var random = Math.floor(Math.random()*numberOfChars.length);
+    var random = Math.floor(Math.random()*userInput.length);
     password += userInput[random];
-    return password;
   }
-
-  function validateCharLength (i) {
-    if (i < 8 || i > 128) {
-      alert("Please enter a number between 8 and 128");
-      return generatePassword()
-    }
-    return numberOfChars
-  }
-
-  console.log (password);
+  return password;
 }
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;}
-
-  
