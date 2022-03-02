e35460179a33e7dbc88292d01a95324aa8af36fd
Zion Yang <zionremnant@gmail.com>
2022-03-02 09:47:04 -0800

    renamed var arrays

--- script.js
+++ script.js
@@ -11,37 +11,34 @@ generateBtn.addEventListener("click", writePassword);
 
 // Assignment Code
 var userInput = []
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numeric = [0,1,2,3,4,5,6,7,8,9];
-var specialCharaters = ["!","@","#","$","%","^","&","*"];
+var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var capitalArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericArray = [0,1,2,3,4,5,6,7,8,9];
+var specialCharatersArray = ["!","@","#","$","%","^","&","*"];
 
-function generatePasswor(){
+function generatePassword(){
   var password = " ";
   numberOfChars = prompt("Please enter a number between 8 and 128")
   if(numberOfChars < 8 || numberOfChars > 128) {
     alert("Please enter a number between 8 and 128");
     return generatePassword;
   }
-  // return numberOfChars
-
-  // validateCharLength(numberOfChars);
 
   var lowercase = confirm("Would you like to include lowercase letters?");
   var capital = confirm("Would you like to include capital letters?");
   var numeric = confirm("Would you like to include numbers?");
   var specialCharaters = confirm("Would you like to include special characters?");
   if(lowercase){
-    userInput = userInput.concat(lowercase)
+    userInput = userInput.concat(lowercaseArray)
   }
   if(capital){
-    userInput = userInput.concat(capital)
+    userInput = userInput.concat(capitalArray)
   }
   if(numeric){
-    userInput = userInput.concat(numeric)
+    userInput = userInput.concat(numericArray)
   }
   if(specialCharaters){
-    userInput = userInput.concat(specialCharaters)
+    userInput = userInput.concat(specialCharatersArray)
   }
 
 console.log(numberOfChars);
